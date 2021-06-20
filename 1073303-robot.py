1f0906afb4fe65b0168d062ed67a41b6c5eb3e8d
1073303 <86176954+1073303@users.noreply.github.com>
2021-06-20 13:21:55 +0800

    Update 1073303-robot.py

--- 1073303-robot.py
+++ 1073303-robot.py
@@ -14,7 +14,7 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSend
 line_bot_api=LineBotApi('7yMZm3UwGAaSNmWMbyiMDaW3jbwNSrptj1yvlXVyY6iitRtcqoW9R2L0KWLWDkpXvxAv897OxbLEqpK7l2afuQBH23kKuSLAJUk3MvxmK/cBLBrOeVkdQFLs4+6OPpkZlrEE2YWDiKI5QRHLk/62IAdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('e8a77aff27140aa956a696e74fd8461b')
 
-@app.route("/callback", methods=['POST'])#建立路由
+@app.route("/callback", methods=['POST'])#建立callback路由，檢查LINE Bot的資料是否正確
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
@@ -24,17 +24,17 @@ def callback():
         abort(400)
     return 'OK'
 
-@handler.add(MessageEvent, message=TextMessage)
+@handler.add(MessageEvent, message=TextMessage)#如果接到使用者傳送的訊息，就將相對應的文字訊息傳回
 def handle_message(event):
-    mtext = event.message.text
+    mtext = event.message.text #取得使用者傳送的文字於mtext變數中
     前期=monoNum(1)+'\n\n'+monoNum(2)
-    if (mtext == '@本期中獎號碼') :
+    if (mtext == '@本期中獎號碼') : #顯示本期中獎號碼(@是特殊指令與一般使用者輸入文字做區別)
         try:
             message = TextSendMessage(text = monoNum(0))
-            line_bot_api.reply_message(event.reply_token,message)
+            line_bot_api.reply_message(event.reply_token,message) #LINE Bot回傳文字訊息
         except:
-            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤!')) 
-    elif (mtext == '@前期中獎號碼') :
+            line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤!')) #LINE Bot發生錯誤時，回傳「發生錯誤」訊息
+    elif (mtext == '@前期中獎號碼') :#顯示前期中獎號碼(@是特殊指令與一般使用者輸入文字做區別)
         try:
             message =TextSendMessage(text = 前期)
             line_bot_api.reply_message(event.reply_token,message)
@@ -45,7 +45,7 @@ def handle_message(event):
             message=TextMessage(text='請輸入發票最後三碼進行對獎！')
         except:
              line_bot_api.reply_message(event.reply_token,TextSendMessage(text='發生錯誤!'))   
-    elif(len(mtext) == 3 and mtext.isdigit()):            
+    elif(len(mtext) == 3 and mtext.isdigit()):     #判斷是否中獎        
         try:
             content=requests.get('https://invoice.etax.nat.gov.tw/invoice.xml')           
             tree = ET.fromstring(content.text) 
